904b0a7af286672d4de84e8140cf301882f142a1
Leo Yim <155793722+Leo12345111@users.noreply.github.com>
2026-03-24 23:37:24 -0700

    Update vocab.js

--- vocab.js
+++ vocab.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Vocabulary Helper
+// @namespace    http://tampermonkey.net/
+// @version      00.00
+// @match        *://*.vocabulary.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @connect      *
+// @run-at       document-start
+// ==/UserScript==
 (function() {
     'use strict';
 
